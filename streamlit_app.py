9ff197881aaf8f7500bd590dd2aa1c8285a735d7
dretzlforsnowflake2 <134234438+dretzlforsnowflake2@users.noreply.github.com>
2023-05-21 16:02:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.header('Breakfast Menu 🍌🥭')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -11,5 +11,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.dataframe(my_fruit_list)
 
+
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.dataframe(my_fruit_list)
