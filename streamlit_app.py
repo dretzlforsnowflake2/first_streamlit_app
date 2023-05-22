bcb1862efbd0d9aaa2ce40d7ef632d0b9b4adf82
dretzlforsnowflake2 <134234438+dretzlforsnowflake2@users.noreply.github.com>
2023-05-21 22:05:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,21 +46,20 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchone()
 
 streamlit.text("Try:")
-try:
-  fruit_choice2 = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice2:
-      streamlit.error("Please Select a fruit to get information")
-  else:
-    back_from_function2 = get_fruityvice_data(fruit_choice2)
-    streamlit.dataframe(back_from_function2)
-except URLError as e:
-  streamlit.error()
+
+#try:
+#  fruit_choice2 = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice2:
+#     streamlit.error("Please Select a fruit to get information")
+#  else:
+#    back_from_function2 = get_fruityvice_data(fruit_choice2)
+#    streamlit.dataframe(back_from_function2)
+#except URLError as e:
+#  streamlit.error()
     
-my_cnx1 = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur1 = my_cnx1.cursor()
-my_cur1.execute("insert into fruit_load_lists values ('from streamlit')")
-
-
+#my_cnx1 = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur1 = my_cnx1.cursor()
+#my_cur1.execute("insert into fruit_load_lists values ('from streamlit')")
 
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
